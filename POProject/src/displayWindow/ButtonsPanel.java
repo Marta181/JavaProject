304689ca15030522698fd293ac42f541b48338b4
Marta181 <62939124+Marta181@users.noreply.github.com>
2020-03-31 14:53:09 +0200

    Update ButtonsPanel.java

--- POProject/src/displayWindow/ButtonsPanel.java
+++ POProject/src/displayWindow/ButtonsPanel.java
@@ -3,22 +3,30 @@ public class ButtonsPanel extends JPanel{
 		super.paintComponent(g);
 		this.setLayout(new GridLayout(1,2));  //this oznacza obiekt tej klasy, który będzie utworzony w main (i każdy kolejny obiekt)
 		
-    JPanel panel1;
-    
-    
+    ButtonsPanel panel1;
+    JButton button1;
+    JButton button2;
+    JButton button3;
+    JButton button4;
+    JButton button5;
+    JButton button6;
+    JButton button7;
+    JButton button8;
+		
     public ButtonsPanel() throws HeadlessException{   //konstruktor
-      panel1 = new JPanel();
+      panel1 = new ButtonsPanel();
       panel1.setBackground(Color.white);
-		  panel1.setSize(500, 400);
-		  panel1.setLayout(new BorderLayoutLayout);
-      
+	panel1.setSize(500, 400);
+	panel1.setLayout(new BorderLayoutLayout);
+      button1 = new JButton ("długość terenu");
+      button2 = new JButton ("szerokość terenu");
     }
 		 
 		
-		JButton button1 = new JButton ("długość terenu");	
+		 	
 		 
 														
-		JButton button2 = new JButton ("szerokość terenu");
+		 
 		
 		
 		JLabel label = new JLabel ("Tutaj bedzie widoczny wygenerowany teren.");

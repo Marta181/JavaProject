cc0698bf8cde571397598ee96e1aec1bc6a48688
Bartosz Ruszczak <bruszczak48@gmail.com>
2020-05-06 14:36:13 +0200

    the very core of terrain generating works

--- TerrainGenerator/src/displayWindow/MeshPanel.java
+++ TerrainGenerator/src/displayWindow/MeshPanel.java
@@ -27,13 +27,17 @@ public class MeshPanel extends GLJPanel implements GLEventListener {
    private GLU glu = new GLU();
    private float rquad = 0.0f;
    
-   int columns = 120;
-   int rows = 100;
-    int scale =5;
+   int columns = 1200;
+   int rows = 1000;
+    int scale =15;
    Random heightRandom = new Random();
    float[][] height= new float[columns][rows];
    
    public long seed;
+   public float smoothness=2f;
+   public float roughness=0.3f;
+   public float maxHeight=70;
+   public int octaves=3;
 
 
 	public MeshPanel() {
@@ -41,7 +45,7 @@ public class MeshPanel extends GLJPanel implements GLEventListener {
 		seed=7347248;
 		for (int x = 0; x < columns; x++) {
 			for (int y = 0; y < rows; y++) {
-				height[x][y]=generateHeight(x, y)*40;
+				height[x][y]=finalHeight(x/smoothness, y/smoothness);
 			}
 		}
 	}
@@ -63,23 +67,33 @@ public class MeshPanel extends GLJPanel implements GLEventListener {
 	     gl.glRotatef( rquad, 0.0f, 1.0f, 1.0f );
 	     gl.glRotatef( -45, 1.0f, 0.0f, 0.0f );
 
-	      gl.glTranslatef( -(columns*scale/2f), (rows*scale/2f), -220.5f );
-	      for(int y=0; y>-rows; y--){ 
+	      gl.glTranslatef( -(columns*scale/2f), (rows*scale/2f), -320.5f );
+	      
+	      for(int y=0; y>-rows+1; y--){ 
 
 	    	  gl.glBegin( GL2.GL_TRIANGLE_STRIP);
 	    	  
 				for (int x = 0; x < columns; x++) {
 					gl.glVertex3f(x*scale, y*scale, height[x][-y]);
-					gl.glVertex3f(x*scale, (y-1)*scale,0);
+					gl.glVertex3f(x*scale, (y-1)*scale,height[x][-y+1]);
 				}
 				
 				gl.glEnd();
 				gl.glFlush();
 				
 	      }
+	      gl.glBegin( GL2.GL_TRIANGLE_STRIP);
+    	  
+			for (int x = 0; x < columns; x++) {
+				gl.glVertex3f(x*scale, (-rows+1)*scale, height[x][rows-1]);
+				gl.glVertex3f(x*scale, -rows*scale,height[x][rows-1]);
+			}
+			
+			gl.glEnd();
+			gl.glFlush();
 	      
 	  
-	      rquad+=1f;
+	      rquad+=0.5f;
 	    }
    
    @Override
@@ -113,10 +127,10 @@ public class MeshPanel extends GLJPanel implements GLEventListener {
 	      
    }
       
-   private float generateHeight(int x, int y) {
+   private float randomHeight(int x, int y) {
 	  
 	   float height=0;
-	   height = randomValue(x,y)*2f+(randomValue(x,y+1)+randomValue(x,y-1)+randomValue(x-1,y)+randomValue(x+1,y))+
+	   height = randomValue(x,y)/2f+(randomValue(x,y+1)+randomValue(x,y-1)+randomValue(x-1,y)+randomValue(x+1,y))+
 			   (randomValue(x+1,y+1)+randomValue(x+1,y-1)+randomValue(x-1,y+1)+randomValue(x-1,y-1))/2f;
 	   return height;
 	   
@@ -125,6 +139,38 @@ public class MeshPanel extends GLJPanel implements GLEventListener {
 	   
 	   heightRandom.setSeed(x*12345+y*4321+seed*54321);
 	   return heightRandom.nextFloat()*2-1;
+    }
+   private float cosineFading(float startInterpolation, float endInterpolation, float factor ) {
+	   
+	   factor = (float)(1f-Math.cos(factor*Math.PI))/2f;
+	   return startInterpolation*(1f-factor) +endInterpolation * factor;
+	   
+	   
+   }
+   private float interpolatedHeight(float x, float y) {
+
+	   float downLeft = randomHeight(((int) x), ((int) y));
+	   float upLeft = randomHeight(((int) x), ((int) y)+1);
+	   float upRight = randomHeight(((int) x)+1, ((int) y)+1);
+	   float downRight = randomHeight(((int) x)+1, ((int) y));
+	   
+	   
+	   float bottomInterpolation = cosineFading(downLeft, downRight,  x-((int) x));
+	   float topInterpolation = cosineFading(upLeft, upRight,  x-((int) x));
+	 
+	   return cosineFading(bottomInterpolation, topInterpolation,  y-((int) y));
    }
 	
+   private float finalHeight(float x, float y) {
+	   float height=0;
+	   
+	   
+	   for(int i=0; i<octaves; i++) {
+		   
+		   float frequency = (float) (Math.pow(2,i)/((float) Math.pow(2, octaves-1)));
+		   height+=interpolatedHeight(x*frequency, y*frequency)*maxHeight*(float)(Math.pow(roughness, i));
+		   
+	   }
+	   return height;
+   }
 }
